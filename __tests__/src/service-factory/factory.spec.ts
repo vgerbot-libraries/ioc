fb39efdfe2619d8dce74972bb0554342ef72bd6b
y1j2x34 <y1j2x34@qq.com>
2022-10-30 17:49:32 +0800

    test: inject value from other service member function

--- __tests__/src/service-factory/factory.spec.ts
+++ __tests__/src/service-factory/factory.spec.ts
@@ -1,4 +1,4 @@
-import { ApplicationContext, Inject } from '../../../src';
+import { ApplicationContext, Factory, Inject } from '../../../src';
 
 describe('ServiceFactory', () => {
     it('should be able to inject value created by factory', () => {
@@ -17,5 +17,29 @@ describe('ServiceFactory', () => {
         const service = context.getInstance(Service);
 
         expect(service.str).toStrictEqual(VALUE);
+    });
+    // eslint-disable-next-line prettier/prettier
+    it('should be able to inject value created by other service\'s member functions', () => {
+        const context = new ApplicationContext();
+        const FACTORY_IDENTIFIER = '';
+        const VALUE = Symbol('value');
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        class ProviderService {
+            @Factory(FACTORY_IDENTIFIER)
+            provider() {
+                return VALUE;
+            }
+        }
+        const consumer = jest.fn((v: symbol) => {
+            return v;
+        });
+        context.invoke(consumer, {
+            injections: [FACTORY_IDENTIFIER]
+        });
+        expect(consumer).toBeCalledWith(VALUE);
+        expect(consumer).toReturnWith(VALUE);
     });
 });
