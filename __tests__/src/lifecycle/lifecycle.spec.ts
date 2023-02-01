33fd6b2e5eb50cdcdec0d1f893611e84e63ab8b4
y1j2x34 <y1j2x34@qq.com>
2023-02-02 00:49:32 +0800

    test: tests the PreInject && PostInject lifecycle

---
+++ __tests__/src/lifecycle/lifecycle.spec.ts
@@ -0,0 +1,59 @@
+import { ApplicationContext, Inject, PreInject, PostInject } from '../../../src';
+
+describe('Lifecycle', () => {
+    describe('PreInject', () => {
+        it('Should methods marked with this lifecycle be called before injection', () => {
+            const testFn = jest.fn();
+            const testService2Fn = jest.fn();
+            class Service2 {
+                constructor() {
+                    testService2Fn();
+                }
+            }
+            class Service {
+                @Inject(Service2)
+                service2!: Service2;
+
+                @PreInject()
+                beforeInjection() {
+                    expect(this.service2).toBeUndefined();
+                    expect(testService2Fn).not.toHaveBeenCalled();
+                    testFn(this.service2);
+                }
+            }
+            const ctx = new ApplicationContext();
+            ctx.getInstance(Service);
+
+            expect(testFn).toHaveBeenCalledTimes(1);
+            expect(testFn).toHaveBeenCalledWith(undefined);
+        });
+    });
+    describe('PostInject', () => {
+        it('Should methods marked with this lifecycle be called after injection', () => {
+            const testFn = jest.fn();
+            const testService2Fn = jest.fn();
+            class Service2 {
+                constructor() {
+                    testService2Fn();
+                }
+            }
+            class Service {
+                @Inject(Service2)
+                service2!: Service2;
+
+                @PostInject()
+                afterInjection() {
+                    expect(this.service2).toBeInstanceOf(Service2);
+                    expect(testService2Fn).toHaveBeenCalled();
+                    testFn(this.service2);
+                }
+            }
+            const ctx = new ApplicationContext();
+            const service = ctx.getInstance(Service);
+
+            expect(testFn).toHaveBeenCalledTimes(1);
+            expect(service.service2).toBeDefined();
+            expect(testFn).toHaveBeenCalledWith(service.service2);
+        });
+    });
+});
