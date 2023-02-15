37b18c387d3c7ac986a6af4427aa5cc43b5f61ab
y1j2x34 <y1j2x34@qq.com>
2023-02-16 01:41:49 +0800

    test: @Bind annotation

---
+++ __tests__/src/bind/bind-annotation.spec.ts
@@ -0,0 +1,20 @@
+import { ApplicationContext, Bind, Inject } from '../../../src';
+
+describe('@Bind', () => {
+    it('should set alias for a component', () => {
+        const SERVICE_ALIAS_IDENTIFIER = 'alias-name-of-service-a';
+        @Bind(SERVICE_ALIAS_IDENTIFIER)
+        class ServiceA {}
+        class ServiceB {
+            @Inject(ServiceA)
+            public serviceA0!: ServiceA;
+            @Inject(SERVICE_ALIAS_IDENTIFIER)
+            public serviceA1!: ServiceA;
+        }
+        const ctx = new ApplicationContext();
+        const serviceB = ctx.getInstance(ServiceB);
+        console.log(serviceB.serviceA0);
+        console.log(serviceB.serviceA1);
+        expect(serviceB.serviceA0).toBe(serviceB.serviceA1);
+    });
+});
