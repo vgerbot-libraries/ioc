875fd933b240c2f62e3bfe50054bb995a304e27d
y1j2x34 <y1j2x34@qq.com>
2023-03-14 23:21:51 +0800

    fix: exclude invalid members

--- src/common/getAllMethodMemberNames.ts
+++ src/common/getAllMethodMemberNames.ts
@@ -1,7 +1,12 @@
 import { Newable } from '../types/Newable';
 
 function getMethodDescriptors(prototype: object): Record<string, PropertyDescriptor> {
-    if (typeof prototype !== 'object' || prototype === null) {
+    if (
+        typeof prototype !== 'object' ||
+        prototype === null ||
+        Object.prototype === prototype ||
+        Function.prototype === prototype
+    ) {
         return {};
     }
     const superPrototype = Object.getPrototypeOf(prototype);
@@ -11,6 +16,7 @@ function getMethodDescriptors(prototype: object): Record<string, PropertyDescrip
 
 export function getAllMethodMemberNames<T>(cls: Newable<T>) {
     const descriptors = getMethodDescriptors(cls.prototype);
+    delete descriptors['constructor'];
     const methodNames = new Set<string>();
     for (const key in descriptors) {
         const member = cls.prototype[key];
