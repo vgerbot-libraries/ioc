cd0e4a56321846f533c27ebbc5961abd60a60281
y1j2x34 <y1j2x34@qq.com>
2023-05-08 21:01:52 +0800

    style: remove useless comments

--- src/common/isNodeJs.ts
+++ src/common/isNodeJs.ts
@@ -1,6 +1,5 @@
 export const isNodeJs = (() => {
     try {
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
         eval('require("os").arch();');
         return true;
     } catch (e) {
