f69cd0ef493d718a2fada6da7ab9f6015ffb7786
y1j2x34 <y1j2x34@qq.com>
2023-05-08 21:00:33 +0800

    refactor: change Object to object

--- src/types/InstantiationAwareProcessor.ts
+++ src/types/InstantiationAwareProcessor.ts
@@ -2,7 +2,7 @@ import { Newable } from './Newable';
 
 export interface InstantiationAwareProcessor {
     beforeInstantiation<T>(constructor: Newable<T>, args: unknown[]): T | undefined | void;
-    afterInstantiation<T extends Object>(instance: T): T;
+    afterInstantiation<T extends object>(instance: T): T;
 }
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface PartialInstAwareProcessor extends Partial<InstantiationAwareProcessor> {
