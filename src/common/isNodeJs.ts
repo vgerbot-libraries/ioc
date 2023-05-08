59990351707738c7d534493902a68545258540e3
y1j2x34 <y1j2x34@qq.com>
2023-05-08 20:58:04 +0800

    fix: check nodejs version

--- src/common/isNodeJs.ts
+++ src/common/isNodeJs.ts
@@ -1,8 +1,7 @@
 export const isNodeJs = (() => {
     try {
         // eslint-disable-next-line @typescript-eslint/no-var-requires
-        const os = require('os');
-        os.arch();
+        eval('require("os").arch();');
         return true;
     } catch (e) {
         return false;
