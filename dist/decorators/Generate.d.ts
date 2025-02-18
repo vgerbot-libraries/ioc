53a009ec1b639832eb7c44bbf2b3d7e3551d12d4
y1j2x34 <y1j2x34@qq.com>
2025-02-18 21:56:24 +0800

    refactor: @Generate decorator

--- dist/decorators/Generate.d.ts
+++ dist/decorators/Generate.d.ts
@@ -1,2 +1,2 @@
 import { ApplicationContext } from '../foundation';
-export declare function Generate<V>(generator: <T>(this: T, appCtx: ApplicationContext) => V): PropertyDecorator;
+export declare function Generate<T, V>(generator: (this: T, appCtx: ApplicationContext) => V): PropertyDecorator;
--- src/decorators/Generate.ts
+++ src/decorators/Generate.ts
@@ -2,13 +2,13 @@ import { ApplicationContext } from '../foundation';
 import { ClassMetadata, GlobalMetadata } from '../metadata';
 import { MetadataInstanceManager } from '../metadata/MetadataInstanceManager';
 
-export function Generate<V>(generator: <T>(this: T, appCtx: ApplicationContext) => V): PropertyDecorator {
+export function Generate<T, V>(generator: (this: T, appCtx: ApplicationContext) => V): PropertyDecorator {
     return (target: Object, propertyKey: string | symbol) => {
         const metadata = MetadataInstanceManager.getMetadata(target.constructor, ClassMetadata);
         const value_symbol = Symbol('');
         metadata.recordPropertyType(propertyKey, value_symbol);
         GlobalMetadata.getInstance().recordFactory(value_symbol, (container, owner) => {
-            return () => generator.call(owner, container);
+            return () => generator.call(owner as T, container);
         });
     };
 }
