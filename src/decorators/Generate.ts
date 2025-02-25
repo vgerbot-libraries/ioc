eb5dbc2440e8f74591312dd8b23c4882812c313a
y1j2x34 <y1j2x34@qq.com>
2025-02-25 14:35:46 +0800

    refactor: @Generate decorator to handle new decorator context support

--- src/decorators/Generate.ts
+++ src/decorators/Generate.ts
@@ -1,14 +1,25 @@
 import { ApplicationContext } from '../foundation';
 import { ClassMetadata, GlobalMetadata } from '../metadata';
 import { MetadataInstanceManager } from '../metadata/MetadataInstanceManager';
+import { Newable } from '../types';
 
 export function Generate<T, V>(generator: (this: T, appCtx: ApplicationContext) => V): PropertyDecorator {
-    return (target: Object, propertyKey: string | symbol) => {
-        const metadata = MetadataInstanceManager.getMetadata(target.constructor, ClassMetadata);
-        const value_symbol = Symbol('');
-        metadata.recordPropertyType(propertyKey, value_symbol);
+    return (target: Object | undefined, propertyKey: string | symbol | ClassMethodDecoratorContext<unknown>) => {
+        if (target === undefined && typeof propertyKey === 'object') {
+            const context = propertyKey;
+            context.addInitializer(function (this: unknown) {
+                defineGenerator(this as Newable<unknown>, context.name);
+            });
+        } else if (target && typeof propertyKey !== 'object') {
+            defineGenerator(target.constructor as Newable<T>, propertyKey);
+        }
+    };
+    function defineGenerator(clazz: Newable<unknown>, memberName: string | symbol) {
+        const metadata = MetadataInstanceManager.getMetadata(clazz, ClassMetadata);
+        const value_symbol = Symbol('@Generate-' + memberName.toString());
+        metadata.recordPropertyType(memberName, value_symbol);
         GlobalMetadata.getInstance().recordFactory(value_symbol, (container, owner) => {
             return () => generator.call(owner as T, container);
         });
-    };
+    }
 }
