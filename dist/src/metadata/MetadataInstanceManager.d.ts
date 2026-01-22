3f6d4fc6f52c142404978a19820975881a8fc935
jianxin_yang <jianxin_yang@foxitsoftware.com>
2026-01-22 13:24:19 +0800

    chore: update dist

--- dist/src/metadata/MetadataInstanceManager.d.ts
+++ dist/src/metadata/MetadataInstanceManager.d.ts
@@ -3,7 +3,7 @@ import 'reflect-metadata';
 type AnyMetadata = Metadata<MetadataReader, unknown>;
 type AnyMetadataClass = MetadataClass<MetadataReader, unknown, AnyMetadata>;
 export declare class MetadataInstanceManager {
-    static getMetadata<R extends MetadataReader, T extends Object, M extends Metadata<R, T> = Metadata<R, T>>(target: T, metadataClass: MetadataClass<R, T, M>): M;
+    static getMetadata<R extends MetadataReader, T extends object, M extends Metadata<R, T> = Metadata<R, T>>(target: T, metadataClass: MetadataClass<R, T, M>): M;
     static getAllInstanceof<M extends AnyMetadataClass>(metadataClass: M): AnyMetadata[];
 }
 export {};
